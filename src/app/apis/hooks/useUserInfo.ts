6a22d6ad3830bf11b41fbbcefea9ef7abac5217a
torong-dev <hi_ryuji@naver.com>
2024-08-03 23:24:39 +0000

    fix: 상태 코드 및 응답 헤더 확인을 생략

--- src/app/apis/hooks/useUserInfo.ts
+++ src/app/apis/hooks/useUserInfo.ts
@@ -11,22 +11,6 @@ export const useUserInfo = () => {
       console.log("사용자 정보를 가져오는 중...");
       const response = await authApi.checkAuth();
 
-      // 상태 코드와 응답 헤더를 출력해 상태를 확인합니다.
-      console.log("HTTP 상태 코드:", response.status);
-
-      console.log("응답 헤더:");
-      for (const [key, value] of response.headers.entries()) {
-        console.log(`${key}: ${value}`);
-      }
-
-      if (!response.ok) {
-        console.error(
-          "사용자 정보를 가져오는데 실패했습니다. 응답 상태가 OK가 아닙니다.",
-          response.statusText,
-        );
-        throw new Error("사용자 정보를 가져오는데 실패했습니다.");
-      }
-
       const data = await response.json();
       console.log("파싱된 데이터:", data);
 
