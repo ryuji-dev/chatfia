e8f23d86eece4d78f3eb36ec49d0d21a161ed651
torong-dev <hi_ryuji@naver.com>
2024-08-18 01:16:07 +0000

    fix: 비밀번호 변경 API 오류 해결

--- src/app/(route)/mypage/job-win-rates/page.tsx
+++ src/app/(route)/mypage/job-win-rates/page.tsx
@@ -1,6 +1,6 @@
 export default function JobWinRatesPage() {
   return (
-    <div>
+    <div className="flex flex-col p-4 pb-20">
       <div>JobWinRates Content Here</div>
     </div>
   );
--- src/app/(route)/mypage/page.tsx
+++ src/app/(route)/mypage/page.tsx
@@ -5,7 +5,7 @@ import UserProfile from "@/app/features/mypage/UserProfile";
 
 export default function MyPage() {
   return (
-    <div className="flex flex-col items-center p-4 pb-20">
+    <div className="flex flex-col p-4 pb-20">
       <JobWinRates />
       <UserProfile />
     </div>
